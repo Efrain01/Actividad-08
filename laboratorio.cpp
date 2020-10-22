1effee8e0162f80afb9bf3e7b7151e2357626538
Efrain Rosas <carlos.rosas7476@alumnos.udg.mx>
2020-10-22 11:02:32 -0500

    implentamos los metodos respaldar_tabla y respaldar

--- laboratorio.cpp
+++ laboratorio.cpp
@@ -1,4 +1,5 @@
 #include "laboratorio.h"
+#include <fstream>
 
 Laboratorio::Laboratorio(){
     cont = 0;
@@ -25,7 +26,36 @@ void Laboratorio::mostrar(){
         cout<< p;
         cout<<endl;
     }
+    
+}
 
-    
-    
+void Laboratorio::respaldar_tabla(){
+    ofstream archivo("computadora.txt");
+    if(archivo.is_open()){
+        cout << left;
+        cout <<setw(20) << "Sistema Operativo";
+        cout <<setw(6) << "RAM";
+        cout <<setw(6) << "ROM";
+        cout <<setw(15) << "Procesador";
+        cout << endl;
+        for (size_t i = 0; i < cont; i++){
+            Computadora &p = arreglo[i];
+            archivo << p;
+        }
+    }
+    archivo.close();
+}
+
+void Laboratorio::respaldar_tabla(){
+    ofstream archivo("computadora.txt");
+    if(archivo.is_open()){
+        for (size_t i = 0; i < cont; i++){
+            Computadora &p = arreglo[i];
+            archivo << p.getOs()<<endl;
+            archivo << p.getRam()<<endl;
+            archivo << p.getRom()<<endl;
+            archivo << p.getProcesador()<<endl;
+        }
+    }
+    archivo.close();
 }
